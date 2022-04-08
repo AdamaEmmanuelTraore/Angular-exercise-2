7cdbb7238063ed451500d7f58de0925106933d73
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2022-04-08 12:45:53 +0200

    Using structural directive like [ngSwitch]

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,12 +6,18 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     template: `
         <div class="info">
         <div><h1>{{event.name}}</h1></div>
-        <div><h3>Data: {{event.data}}</h3></div>
-        <div><h3>Price: {{event.price}}</h3></div>
-        <div><h3>Image: {{event.imageUrl}}</h3></div>
-        <div><h3>Adress: {{event.location.adress}}</h3></div>
-        <div><h3>City: {{event.location.city}}</h3></div>
-        <div><h3>Country: {{event.location.country}}</h3></div>
+        <div><h3>Data: {{event?.data}}</h3></div>
+        <div [ngSwitch]="event?.time">
+            <h3>Time: {{event?.time}}</h3>
+            <h3><span *ngSwitchCase="'8:00 am'">(Early start)</span></h3>
+            <h3><span *ngSwitchCase="'10:00 am'">(Late start)</span></h3>
+            <h3><span *ngSwitchDefault>(Normal start)</span></h3>
+        </div>
+        <div><h3>Price: {{event?.price}}</h3></div>
+        <div><h3>Image: {{event?.imageUrl}}</h3></div>
+        <div><h3>Adress: {{event?.location.address}}</h3></div>
+        <div><h3>City: {{event?.location.city}}</h3></div>
+        <div><h3>Country: {{event?.location.country}}</h3></div>
     </div>
     `
 })
