6472b92ba5be52be7b927d842314d8903c05e870
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2022-04-08 13:02:36 +0200

    Angular project

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,20 +4,20 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
     selector: 'event-thumbnail',
     template: `
-        <div class="info">
+    <div class="info">
         <div><h1>{{event.name}}</h1></div>
-        <div><h3>Data: {{event?.data}}</h3></div>
+        <div>Data: {{event?.data}}</div>
         <div [ngSwitch]="event?.time">
-            <h3>Time: {{event?.time}}</h3>
-            <h3><span *ngSwitchCase="'8:00 am'">(Early start)</span></h3>
-            <h3><span *ngSwitchCase="'10:00 am'">(Late start)</span></h3>
-            <h3><span *ngSwitchDefault>(Normal start)</span></h3>
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+            <span *ngSwitchDefault>(Normal start)</span>
         </div>
-        <div><h3>Price: {{event?.price}}</h3></div>
-        <div><h3>Image: {{event?.imageUrl}}</h3></div>
-        <div><h3>Adress: {{event?.location.address}}</h3></div>
-        <div><h3>City: {{event?.location.city}}</h3></div>
-        <div><h3>Country: {{event?.location.country}}</h3></div>
+        <div>Price: {{event?.price}}</div>
+        <div>Image: {{event?.imageUrl}}</div>
+        <div>Adress: {{event?.location.address}}</div>
+        <div>City: {{event?.location.city}}</div>
+        <div>Country: {{event?.location.country}}</div>
     </div>
     `
 })
