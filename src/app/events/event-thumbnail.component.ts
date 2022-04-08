52b16a3b7d681ea358fd69bb59902e843b7fa5f5
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2022-04-08 10:10:25 +0200

    Update with structural directive ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,23 +1,22 @@
-import { Component, Input } from "@angular/core";
+// COMPONENTE FIGLIO
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-    selector: '<event-thumbnail>',
+    selector: 'event-thumbnail',
     template: `
         <div class="info">
-        <h1>{{event.name}}</h1>
-        <h3>Data: {{event.data}}</h3>
-        <h3>Price: {{event.price}}</h3>
-        <h3>Image: {{event.imageUrl}}</h3>
-        <h3>Adress: {{event.location.adress}}</h3>
-        <h3>City: {{event.location.city}}</h3>
-        <h3>Country: {{event.location.country}}</h3>
+        <div><h1>{{event.name}}</h1></div>
+        <div><h3>Data: {{event.data}}</h3></div>
+        <div><h3>Price: {{event.price}}</h3></div>
+        <div><h3>Image: {{event.imageUrl}}</h3></div>
+        <div><h3>Adress: {{event.location.adress}}</h3></div>
+        <div><h3>City: {{event.location.city}}</h3></div>
+        <div><h3>Country: {{event.location.country}}</h3></div>
     </div>
     `
 })
 
 export class EventThumbnailComponent{
-    /* INPUT È UN DECORATORE CHE DICE AD
-    ANGULAR CHE QUESTO EVENTO VERRA' PASSATO
-    AD UN ALTRO COMPONENTE */
+    /* INPUT È UN DECORATORE CHE DICE AD ANGULAR CHE QUESTO EVENTO VERRA' PASSATO AD UN ALTRO COMPONENTE */
     @Input() event: any
 }
