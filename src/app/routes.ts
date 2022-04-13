14df14bb1eeb28c9c767548de9d05887eab6869f
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2022-04-13 16:36:12 +0200

    Creating user profile and used childModule

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -12,5 +12,11 @@ export const appRoutes: Routes = [
     {path:'events', component: EventsListComponent, resolve: {events: EventListResolver}},
     {path: 'events/:id', component: EventDetails, canActivate: [EventRouteActivator]},
     {path: '404', component: Error404Component},
-    {path: '', redirectTo: '/events', pathMatch: 'full'}
+    {path: '', redirectTo: '/events', pathMatch: 'full'},
+
+    // QUI FACCIO L'IMPORTAZIONE DINAMICA 
+    {
+        path: 'user',
+        loadChildren: () => import('./user/user.module').then(m=> m.UserModule)
+    }
 ]
